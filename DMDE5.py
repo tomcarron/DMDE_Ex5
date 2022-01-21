781bd5cf223c19dfb4f5d7ac8691f1903953a259
tomcarron <tcarron@tcd.ie>
2022-01-21 22:02:26 +0100

    Added plots and data directories. Also added part d)

--- DMDE5.py
+++ DMDE5.py
@@ -17,7 +17,7 @@ print("A fizzician")
 """
 Problem 1: Type 1a Supernovae
 """
-file=open("SN_z_mV.txt","r")
+file=open("data/SN_z_mV.txt","r")
 lines=file.readlines()
 name=[]
 z=[]
@@ -66,6 +66,7 @@ plt.plot(z2,node.distmod(z2),'--',color="orange",label="No DE")
 plt.xlabel('$z$')
 plt.ylabel('$(m_V - M_V)$')
 plt.legend()
+plt.savefig('plots/fig0.png',dpi=400,bbox_inches='tight')
 
 '''
 Problem 2: The CMB
@@ -76,7 +77,7 @@ multipole moment l on the x-axis. Note that the columns in the downloaded .dat f
 correspond to l, T T, EE and T E respectively. Make sure to note the units of power.
 '''
 
-file=open("defaults.dat","r")
+file=open("data/defaults.dat","r")
 lines=file.readlines()
 l=[]
 TT=[]
@@ -100,6 +101,7 @@ plt.plot(l,TE,label='TE')
 plt.xlabel('l')
 plt.ylabel('TT/EE/TE')
 plt.legend()
+plt.savefig('plots/fig1.png',dpi=400,bbox_inches='tight')
 
 '''
 With our defaults, plot the temperature and polarization power spectra on their own
@@ -110,23 +112,168 @@ plt.plot(l,TT,label='TT')
 plt.xlabel('l')
 plt.ylabel('TT')
 plt.legend()
+plt.savefig('plots/fig2.png',dpi=400,bbox_inches='tight')
 
 plt.figure(3)
 plt.plot(l,EE,label='EE')
 plt.xlabel('l')
 plt.ylabel('EE')
 plt.legend()
+plt.savefig('plots/fig3.png',dpi=400,bbox_inches='tight')
 
 plt.figure(4)
 plt.plot(l,TE,label='TE')
 plt.xlabel('l')
 plt.ylabel('TE')
 plt.legend()
+plt.savefig('plots/fig4.png',dpi=400,bbox_inches='tight')
 
+'''
+ Modify only the values of Ωb h2 to be in the range from 0.01 to 0.1. Take three values
+other than the default value. Plot the resultant spectra for temperature and polar-
+ization together with the default value results. Explain the differences that you see.
+'''
+file2=open("data/0.01.dat","r")
+lines2=file2.readlines()
+l2=[]
+TT2=[]
+EE2=[]
+TE2=[]
+for x in lines2:
+    l2.append(float(x.split()[0]))
+    TT2.append(float(x.split()[1]))
+    EE2.append(float(x.split()[2]))
+    TE2.append(float(x.split()[3]))
+file2.close()
 
+file3=open("data/0.1.dat","r")
+lines3=file3.readlines()
+l3=[]
+TT3=[]
+EE3=[]
+TE3=[]
+for x in lines3:
+    l3.append(float(x.split()[0]))
+    TT3.append(float(x.split()[1]))
+    EE3.append(float(x.split()[2]))
+    TE3.append(float(x.split()[3]))
+file3.close()
 
+file4=open("data/0.05.dat","r")
+lines4=file4.readlines()
+l4=[]
+TT4=[]
+EE4=[]
+TE4=[]
+for x in lines4:
+    l4.append(float(x.split()[0]))
+    TT4.append(float(x.split()[1]))
+    EE4.append(float(x.split()[2]))
+    TE4.append(float(x.split()[3]))
+file4.close()
 
+plt.figure(5)
+plt.plot(l,TT,label='$\Omega_b h^2=0.0226$(default)')
+plt.plot(l2,TT2,label='$\Omega_b h^2=0.01$')
+plt.plot(l3,TT3,label='$\Omega_b h^2=0.1$')
+plt.plot(l4,TT4,label='$\Omega_b h^2=0.05$')
+plt.xlabel('l')
+plt.ylabel('TT')
+plt.legend()
+plt.savefig('plots/fig5.png',dpi=400,bbox_inches='tight')
 
+plt.figure(6)
+plt.plot(l,EE,label='$\Omega_b h^2=0.0226$(default)')
+plt.plot(l2,EE2,label='$\Omega_b h^2=0.01$')
+plt.plot(l3,EE3,label='$\Omega_b h^2=0.1$')
+plt.plot(l4,EE4,label='$\Omega_b h^2=0.05$')
+plt.xlabel('l')
+plt.ylabel('EE')
+plt.legend()
+plt.savefig('plots/fig6.png',dpi=400,bbox_inches='tight')
+
+plt.figure(7)
+plt.plot(l,TE,label='$\Omega_b h^2=0.0226$(default)')
+plt.plot(l2,TE2,label='$\Omega_b h^2=0.01$')
+plt.plot(l3,TE3,label='$\Omega_b h^2=0.1$')
+plt.plot(l4,TE4,label='$\Omega_b h^2=0.05$')
+plt.xlabel('l')
+plt.ylabel('TE')
+plt.legend()
+plt.savefig('plots/fig7.png',dpi=400,bbox_inches='tight')
+
+'''
+d) Repeat everything from part (c), but this time change Ωbh2 back to the default and
+change only Ωc h2 to any three other values between 0.05 and 0.2.
+'''
+file5=open("data/c_0.05.dat","r")
+lines5=file5.readlines()
+l5=[]
+TT5=[]
+EE5=[]
+TE5=[]
+for x in lines5:
+    l5.append(float(x.split()[0]))
+    TT5.append(float(x.split()[1]))
+    EE5.append(float(x.split()[2]))
+    TE5.append(float(x.split()[3]))
+file5.close()
+
+file6=open("data/c_0.15.dat","r")
+lines6=file6.readlines()
+l6=[]
+TT6=[]
+EE6=[]
+TE6=[]
+for x in lines6:
+    l6.append(float(x.split()[0]))
+    TT6.append(float(x.split()[1]))
+    EE6.append(float(x.split()[2]))
+    TE6.append(float(x.split()[3]))
+file6.close()
+
+file7=open("data/c_0.2.dat","r")
+lines7=file7.readlines()
+l7=[]
+TT7=[]
+EE7=[]
+TE7=[]
+for x in lines7:
+    l7.append(float(x.split()[0]))
+    TT7.append(float(x.split()[1]))
+    EE7.append(float(x.split()[2]))
+    TE7.append(float(x.split()[3]))
+file7.close()
+
+plt.figure(8)
+plt.plot(l,TT,label='$\Omega_c h^2=0.112$(default)')
+plt.plot(l5,TT5,label='$\Omega_c h^2=0.05$')
+plt.plot(l6,TT6,label='$\Omega_c h^2=0.15$')
+plt.plot(l7,TT7,label='$\Omega_c h^2=0.2$')
+plt.xlabel('l')
+plt.ylabel('TT')
+plt.legend()
+plt.savefig('plots/fig8.png',dpi=400,bbox_inches='tight')
+
+plt.figure(9)
+plt.plot(l,EE,label='$\Omega_c h^2=0.112$(default)')
+plt.plot(l5,EE5,label='$\Omega_c h^2=0.05$')
+plt.plot(l6,EE6,label='$\Omega_c h^2=0.15$')
+plt.plot(l7,EE7,label='$\Omega_c h^2=0.2$')
+plt.xlabel('l')
+plt.ylabel('EE')
+plt.legend()
+plt.savefig('plots/fig9.png',dpi=400,bbox_inches='tight')
+
+plt.figure(10)
+plt.plot(l,TE,label='$\Omega_c h^2=0.112$(default)')
+plt.plot(l5,TE5,label='$\Omega_c h^2=0.05$')
+plt.plot(l6,TE6,label='$\Omega_c h^2=0.15$')
+plt.plot(l7,TE7,label='$\Omega_c h^2=0.2$')
+plt.xlabel('l')
+plt.ylabel('TE')
+plt.legend()
+plt.savefig('plots/fig10.png',dpi=400,bbox_inches='tight')
 
 
 plt.show()
